1b1d8324be18a2f64ed8dfed92553aee20afe781
Xebastiano <sebastiangarciacastaneda@gmail.com>
2019-04-14 22:45:52 -0500

    Mi Proyecto Personal de C#

---
+++ ProyectoPersonalVisual/ProyectoPersonalVisual/Game.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoPersonalVisual{
+    class Game{
+        public int Vidas = 5;
+        public string SecretWord;
+        public string ShownWord { get; private set; }
+        public const string Hider = "*";
+        public const char Space = ' ';
+        public Game() {
+
+        }
+    }
+}
